b16e5e59aac297d18a1f487c0bec3bb7f900dee0
Dilan <dilan.uriostegui@gmail.com>
2021-05-11 13:32:49 -0500

    ImplementaciÃón de la autentificacion por Google y por Github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
 
+import { ChatService } from "./providers/chat.service"
 
 @Component({
   selector: 'app-root',
@@ -11,9 +10,6 @@ import { Observable } from 'rxjs';
 export class AppComponent {
   
   
-  public chats: Observable<any[]>;
-  constructor(firestore: AngularFirestore) {
-    this.chats = firestore.collection('chats').valueChanges();
-  }
+  constructor( public _cs: ChatService ) {}
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,16 @@ import { environment } from '../environments/environment';
 
 //Componentes
 import { ChatComponent } from './components/chat/chat.component';
+import { LoginComponent } from './components/login/login.component'
 
 //Servicios
-import { ChatService } from "./providers/chat.service"
+import { ChatService } from "./providers/chat.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent
+    ChatComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
